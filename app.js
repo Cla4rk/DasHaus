96ca0a3b76b1f70cc7bf83ab2df8ee2d1399feb4
Cla4rk <santiagovalero99@gmail.com>
2022-05-27 17:47:46 -0300

    terminaciones y seccion personalizacion terminada

--- app.js
+++ app.js
@@ -14,20 +14,69 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    
+    $(document).ready(function(){
+    $(window).scrollTop(0);
+    });
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
+    // Menu Toggler
     const menuToggle = document.querySelector('.toggle');
-      const showcase = document.querySelector('.showcase');
+    const showcase = document.querySelector('.showcase');
 
-      menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
-        showcase.classList.toggle('active');
+    menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
       })
+      $(document).ready(function(){
+	$('.toggle').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+//SmoothScrolling
+    SmoothScroll({
+        // Scrolling Core
+        animationTime: 800, // [ms]
+        stepSize: 100, // [px]
+
+        // Acceleration
+        accelerationDelta: 50,  // 50
+        accelerationMax: 3,   // 3
+
+        // Keyboard Settings
+        keyboardSupport: true,  // option
+        arrowScroll: 50,    // [px]
+
+        // Pulse (less tweakable)
+        // ratio of "tail" to "acceleration"
+        pulseAlgorithm: true,
+        pulseScale: 4,
+        pulseNormalize: 1,
+
+        // Other
+        touchpadSupport: false, // ignore touchpad by default
+        fixedBackground: true,
+        excluded: ''
+    })
+    // Reveal
+    window.addEventListener('scroll',reveal);
+
+    function reveal(){
+        var reveals = document.querySelectorAll('.reveal');
+
+        for(var i = 0; i < reveals.length; i ++){
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[i]. getBoundingClientRect().top;
+            var revealpoint = 150;
+
+            if(revealtop < windowheight - revealpoint){
+                reveals[i].classList.add('active');
+            }
+        }
+    }
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
